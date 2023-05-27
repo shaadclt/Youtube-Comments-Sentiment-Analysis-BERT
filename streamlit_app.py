48ac987717f648890dc02c7fb1b3cc7594a6a400
Mohamed Shaad <98437584+shaadclt@users.noreply.github.com>
2023-05-27 09:14:18 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,38 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
-import re
-import time
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 import pandas as pd
 
-st.title("Youtube Comments Sentiment Analysis")
+def sentiment_analysis():
+    st.title("Youtube Comments Sentiment Analysis")
 
-tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+    url = st.text_input("Enter the Youtube Video URL")
 
-# Get user input
-url = st.text_input("Enter the Youtube Video URL")
+    if st.button("Analyze"):
+        if url:
+            tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+            model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
+            response = requests.get(url)
+            data = response.text
 
-# Process user input and display output
-if url:
-    # Get the content of the URL
+            df = pd.DataFrame({'comment': [data]})
 
-    data=[]
+            def sentiment_score(comment):
+                tokens = tokenizer.encode(comment, return_tensors='pt')
+                result = model(tokens)
+                return int(torch.argmax(result.logits)) + 1
 
-    with Chrome(executable_path=r'C:\Program Files\chromedriver.exe') as driver:
-        wait = WebDriverWait(driver,15)
-        driver.get(url)
+            df['sentiment'] = df['comment'].apply(lambda x: sentiment_score(x[:512]))
 
-        for item in range(1): 
-            wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-            time.sleep(15)
+            avg_sentiment_score = round(df['sentiment'].mean(), 2)
 
-        for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
-            data.append(comment.text)
+            st.success(f"Average Sentiment Score: {avg_sentiment_score}")
+
+if __name__ == '__main__':
+    sentiment_analysis()
     
-    df = pd.DataFrame(data, columns=['comment'])
-    df.head()
-
-    def sentiment_score(comment):
-        tokens = tokenizer.encode(comment, return_tensors='pt')
-        result = model(tokens)
-        return int(torch.argmax(result.logits))+1
-    
-    df['sentiment'] = df['comment'].apply(lambda x:sentiment_score(x[:512]))
-
-    avg_sentiment_score = round(df['sentiment'].mean(),2)
-
-
-    # Display the summary
-    st.subheader("Sentiment Score out of 5 (1 : Lowest, 5 : Highest):")
-    st.write(avg_sentiment_score)
-    
-
-
 footer = """
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <footer>
